f5b27334cd953032ec36dec6df9df340a76ae341
Tim Shuvalov <tashuvalov@gmail.com>
2024-11-23 16:25:14 +0300

    bullet projectile effect and hit effect

--- Assets/Scripts/UnitAnimator.cs
+++ Assets/Scripts/UnitAnimator.cs
@@ -8,6 +8,12 @@ public class UnitAnimator : MonoBehaviour
 	[SerializeField]
 	private Animator animator;
 	
+	[SerializeField]
+	private Transform bulletProjectilePrefab;
+	
+	[SerializeField]
+	private Transform shootPointTransform;
+	
 	private void Awake()
 	{
 		if(TryGetComponent<MoveAction>(out MoveAction moveAction))
@@ -23,12 +29,19 @@ public class UnitAnimator : MonoBehaviour
 		}
 	}
 
-    private void ShootAction_OnShoot(object sender, EventArgs e)
-    {
-        animator.SetTrigger("Shoot");
-    }
+	private void ShootAction_OnShoot(object sender, ShootAction.OnShootEventArgs e)
+	{
+		animator.SetTrigger("Shoot");
+		
+		Transform bulletProjectileTransform = Instantiate(bulletProjectilePrefab, shootPointTransform.position, Quaternion.identity);
+		
+		Vector3 targetUnitShootAtPosition = e.targetUnit.GetWorldPosition();
+		targetUnitShootAtPosition.y = shootPointTransform.position.y;
+		
+		bulletProjectileTransform.GetComponent<BulletProjectile>().Setup(targetUnitShootAtPosition);
+	}
 
-    private void MoveAction_OnStartMoving(object sender, EventArgs e)
+	private void MoveAction_OnStartMoving(object sender, EventArgs e)
 	{
 		animator.SetBool("IsWalking", true);
 	}
