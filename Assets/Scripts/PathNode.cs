1f206e13a4cb35046cfc481c38767cbf909bd83c
Tim Shuvalov <tashuvalov@gmail.com>
2025-01-09 14:48:25 +0300

    pathfinding system preparations

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+	private GridPosition gridPosition;
+	
+	private int gCost;
+	private int hCost;
+	private int fCost;
+	
+	private PathNode cameFromPathNode;
+	
+	public PathNode(GridPosition gridPosition)
+	{
+		this.gridPosition = gridPosition;
+	}
+	
+	public override string ToString()
+	{
+		return $"{gridPosition.ToString()}";
+	}
+
+	public int GetGCost()
+	{
+		return gCost;
+	}
+	
+	public int GetHCost()
+	{
+		return gCost;
+	}
+	
+	public int GetFCost()
+	{
+		return fCost;
+	}
+}
