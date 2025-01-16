ef03059c8292118c0a5f0f55a5e020dfbe91a7aa
Tim Shuvalov <tashuvalov@gmail.com>
2025-01-16 19:20:47 +0300

    inpuut refactooring 2

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,10 +1,14 @@
+#define USE_NEW_INPUT_SYSTEM
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
 	public static InputManager Instance {get; private set;}
+	
+	private PlayerInputActions playerInputActions;
 	
 	private void Awake()
 	{
@@ -16,14 +20,34 @@ public class InputManager : MonoBehaviour
 		}
 		
 		Instance = this;
+		
+		playerInputActions = new PlayerInputActions();
+		playerInputActions.Player.Enable();
 	}
-	
-	public Vector2 GetMouseScreenPosition() => Input.mousePosition;
-	
-	public bool IsMouseButtonDown() => Input.GetMouseButtonDown(0);
-	
+
+	public Vector2 GetMouseScreenPosition()
+	{
+#if USE_NEW_INPUT_SYSTEM
+		return Mouse.current.position.ReadValue();
+#else
+		return Input.mousePosition;
+#endif
+	}
+
+	public bool IsMouseButtonDownThisFrame()
+	{
+#if USE_NEW_INPUT_SYSTEM
+		return playerInputActions.Player.Click.WasPressedThisFrame();
+#else
+		return Input.GetMouseButtonDown(0);
+#endif
+	}
+
 	public Vector2 GetCameraMoveVector()
 	{
+#if USE_NEW_INPUT_SYSTEM
+		return playerInputActions.Player.CameraMovement.ReadValue<Vector2>();
+#else
 		Vector2 inputMoveDir = new Vector2(0, 0);
 
 		if (Input.GetKey(KeyCode.W))
@@ -47,10 +71,14 @@ public class InputManager : MonoBehaviour
 		}
 		
 		return inputMoveDir;
+#endif
 	}
 	
 	public float GetCameraRotateAmount()
 	{
+#if USE_NEW_INPUT_SYSTEM
+		return playerInputActions.Player.CameraRotate.ReadValue<float>();
+#else
 		float rotateAmount = 0f;
 		
 		if (Input.GetKey(KeyCode.Q))
@@ -64,10 +92,14 @@ public class InputManager : MonoBehaviour
 		}
 		
 		return rotateAmount;
+#endif
 	}
 	
 	public float GetCameraZoomAmount()
 	{
+#if USE_NEW_INPUT_SYSTEM
+		return playerInputActions.Player.CameraZoom.ReadValue<float>();
+#else
 		float zoomAmount = 0f;
 		
 		if (Input.mouseScrollDelta.y > 0)
@@ -81,5 +113,6 @@ public class InputManager : MonoBehaviour
 		}
 		
 		return zoomAmount;
+#endif
 	}
 }
--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -65,7 +65,7 @@ public class UnitActionSystem : MonoBehaviour
 	
 	private void HandleSelectedAction()
 	{
-		if(InputManager.Instance.IsMouseButtonDown())
+		if(InputManager.Instance.IsMouseButtonDownThisFrame())
 		{
 			GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
 			if(!_selectedAction.IsValidActionGridPosition(mouseGridPosition))
@@ -97,7 +97,7 @@ public class UnitActionSystem : MonoBehaviour
 	
 	private bool TryHandleUnitSelection()
 	{
-		if(InputManager.Instance.IsMouseButtonDown())
+		if(InputManager.Instance.IsMouseButtonDownThisFrame())
 		{
 			Ray ray = Camera.main.ScreenPointToRay(InputManager.Instance.GetMouseScreenPosition());
 			if(Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _unitLayerMask))
